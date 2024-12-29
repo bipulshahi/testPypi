9312b983e19883b89bbf6ad7860b64104423525c
Bipul Shahi <bipulshahi25@gmail.com>
2024-12-29 13:59:32 +0530

    Add files via upload

---
+++ hello_world/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='hello_world_bipul',
+      version='0.0.1',
+      description='A hello world example package',
+      author='Bipul',
+      author_email='bipul@python.net',
+      url='https://www.bipul.com',
+      packages=find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent"
+      ]
+     )
